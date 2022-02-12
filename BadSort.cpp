6d6d0286c0da84bc47a2eba751a3731b63d8e9a8
Jesse <jessegamen@gmail.com>
2022-02-12 13:04:14 +0100

    Update BadSort.cpp

--- BadSort.cpp
+++ BadSort.cpp
@@ -1,10 +1,14 @@
 /*
-    Jesse's inefficient as fuck sorting algorthim for cpp :)
+    Jesse inefficient as fuck sorting algorthim for cpp :)
 */
 
 #include <iostream>
+#include <string>
+#include <map>
 
 using std::cout;
+using std::string;
+using std::map;
 
 void swap(int &a, int &b)
 {
@@ -15,7 +19,7 @@ void swap(int &a, int &b)
 
 bool sortOnce(int * arr, int size)
 {
-    cout << "Do it again\n";
+    // cout << "Do it again\n";
     bool done = true;
     
     for (int i = 1; i < size; i++) {
@@ -30,7 +34,7 @@ bool sortOnce(int * arr, int size)
 
 void sort(int * arr, int size)
 {
-    if (size < 2) return;
+    if (size < 2) return;                       // Return when only 1 element in array.
     
     while (!sortOnce(arr, size));
 }
@@ -42,6 +46,21 @@ void printNumbers(int * arr, int size)
     }
 }
 
+map<char, unsigned int> createCharacterMap(string str, int size)
+{
+    map<char, unsigned int> myMap;
+    unsigned int cCount['z' - 'A'];                 // 'z' - 'A' results in amount of letters we check for
+    
+    for (int c = 'A'; c < 'z'; c++) {                          // Can also use (auto & c : str) but then index has to be formula: c - 'z'
+        for (auto & letter : str) {
+            if(letter == c) cCount[c - 'A']++;
+        }
+        cout << (char) c << ": " << cCount[c- 'A'];
+    }
+    
+    
+}
+
 int main()
 {
     int numbers[] = { 12, 2, 45, 19, 37, 8 , 5, 3, 4, 897213, 993, 23628, 7378, 3883, 88 };
@@ -49,5 +68,15 @@ int main()
     sort(numbers, sizeof(numbers) / sizeof(int));
     printNumbers(numbers, sizeof(numbers) / sizeof(int));
     
+    string scentence = "I'm a scentence. And I'm another scentence";
+    
+    map<char, unsigned int> charmap;
+    charmap.insert(std::pair<char, unsigned int>('a', 5));
+    charmap.insert(std::pair<char, unsigned int>('b', 7));
+    charmap.insert(std::pair<char, unsigned int>('c', 5));
+    
+    for (auto & item : charmap) {
+        cout << item.first << " | " << item.second << std::endl;
+    }
     return 0;
 }
