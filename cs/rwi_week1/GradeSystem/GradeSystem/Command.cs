73270141d1c69817d866965e769fa12b9c3dc297
Jessebeau2001 <jessegamen@gmail.com>
2022-05-04 11:46:52 +0200

    Create Command.cs

---
+++ cs/rwi_week1/GradeSystem/GradeSystem/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeSystem
+{
+    public struct Command
+    {
+        public Command(string[] args)
+        {
+            
+        }
+
+
+        private static T GetEnum<T>(string str) where T : struct, Enum
+        {
+            Enum.TryParse<T>(str, out var r);
+            return r;
+        }
+        
+        
+        private enum Option  { Student, Grade, Help, Exit  }
+        private enum ParamStudent { List, Edit, Add, Delete}
+        private enum ParamGrade { Find, Edit, Average, Freeze}
+    }
+}
