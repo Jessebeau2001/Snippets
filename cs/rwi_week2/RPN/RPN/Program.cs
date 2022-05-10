cec7a510289e7c02d9e4adf54795c1085545f897
Jesse <jessegamen@gmail.com>
2022-05-10 17:16:01 +0200

    Messing around

--- cs/rwi_week2/RPN/RPN/Program.cs
+++ cs/rwi_week2/RPN/RPN/Program.cs
@@ -6,42 +6,7 @@ namespace RPN
     {
         private static void Main()
         {
-            var sens = new DistanceSensor(7);
-            sens.Write();
-        }
-    }
-
-    internal interface IWriteable
-    {
-        public void Write();
-    }
-
-    internal abstract class Sensor : IWriteable
-    {
-        private int _pin;
-        protected Sensor(int pin)
-        {
-            _pin = pin;
-        }
-        
-        protected abstract void Update();
-        
-        public void Write()
-        {
-            Console.WriteLine($"The pin is {_pin}");
-        }
-    }
-
-    internal class DistanceSensor : Sensor
-    {
-        public DistanceSensor(int pin) : base(pin)
-        {
             
-        }
-
-        protected override void Update()
-        {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ cs/rwi_week2/RPN/RPN/Token.cs
@@ -0,0 +1,16 @@
+﻿namespace RPN
+{
+    /*public class Token<T> where T : Enum
+    {
+        public Token(T input)
+        {
+            
+        }
+    }*/
+
+    public struct Token<T>
+    {
+        public T Value { get; set; }
+    }
+}
+
