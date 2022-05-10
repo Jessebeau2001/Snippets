0197395697cc5f31c3dbc8c4b4ea52ab37d0e195
Jesse <jessegamen@gmail.com>
2022-05-10 16:04:04 +0200

    Push

--- cs/rwi_week1/GradeSystem/GradeSystem/Command.cs
+++ cs/rwi_week1/GradeSystem/GradeSystem/Command.cs
@@ -1,25 +1,14 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 
 namespace GradeSystem
 {
     public struct Command
     {
-        public Command(string[] args)
-        {
-            
-        }
-
-
         private static T GetEnum<T>(string str) where T : struct, Enum
         {
             Enum.TryParse<T>(str, out var r);
             return r;
         }
-        
-        
-        private enum Option  { Student, Grade, Help, Exit  }
-        private enum ParamStudent { List, Edit, Add, Delete}
-        private enum ParamGrade { Find, Edit, Average, Freeze}
     }
-}
+}*/
---
+++ cs/rwi_week2/RPN/RPN/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace RPN
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var sens = new DistanceSensor(7);
+            sens.Write();
+        }
+    }
+
+    internal interface IWriteable
+    {
+        public void Write();
+    }
+
+    internal abstract class Sensor : IWriteable
+    {
+        private int _pin;
+        protected Sensor(int pin)
+        {
+            _pin = pin;
+        }
+        
+        protected abstract void Update();
+        
+        public void Write()
+        {
+            Console.WriteLine($"The pin is {_pin}");
+        }
+    }
+
+    internal class DistanceSensor : Sensor
+    {
+        public DistanceSensor(int pin) : base(pin)
+        {
+            
+        }
+
+        protected override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
