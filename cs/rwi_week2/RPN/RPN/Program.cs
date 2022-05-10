ee4d86ab7faec8166766a9ada399721e514fa8ba
Jessebeau2001 <jessegamen@gmail.com>
2022-05-10 18:43:46 +0200

    Push

--- cs/rwi_week2/RPN/RPN/Program.cs
+++ cs/rwi_week2/RPN/RPN/Program.cs
@@ -6,7 +6,13 @@ namespace RPN
     {
         private static void Main()
         {
+            var aToken = new Token<char>('+');
+            var bToken = new Token<int>(12);
             
+            Console.WriteLine(aToken);
+            Console.WriteLine(bToken);
+
+            var list = new List<Token<IToken>>();
         }
     }
 }
--- cs/rwi_week2/RPN/RPN/Token.cs
+++ cs/rwi_week2/RPN/RPN/Token.cs
@@ -1,16 +1,24 @@
 ﻿namespace RPN
 {
-    /*public class Token<T> where T : Enum
+    // public class Token<T> where T : Enum
+    
+    public interface IToken {}
+    public readonly struct Token<T> : IToken
     {
-        public Token(T input)
+        private enum Signature { Number, Operator }
+        private T Value { get; }
+        private Signature Sig { get; }
+        
+        public Token(T value)
         {
-            
+            Value = value;
+            Sig = Value is int ? Signature.Number : Signature.Operator;
         }
-    }*/
 
-    public struct Token<T>
-    {
-        public T Value { get; set; }
+        public override string ToString()
+        {
+            return new string($"(Token: '{Value}', Signature: '{Sig}')");
+        }
     }
 }
 
