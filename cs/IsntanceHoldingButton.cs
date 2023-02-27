e8b06e8d2b3fbd642ccebe25c54f0ea925f01bad
Jesse <jessegamen@gmail.com>
2023-02-27 19:30:41 +0100

    Create IsntanceHoldingButton.cs

---
+++ cs/IsntanceHoldingButton.cs
@@ -0,0 +1,56 @@
+public class InstanceHoldingButton<T> : Button where T : Form, new()
+	{
+		private IContainer components = null;
+		private T? _instance = null;
+
+		public InstanceHoldingButton()
+		{
+			InitializeComponent();
+		}
+
+		public InstanceHoldingButton(IContainer container)
+		{
+			container.Add(this);
+
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+		}
+
+		private void RemoveReference(object? sender, FormClosedEventArgs e)
+		{
+			if (_instance != null)
+			{
+				_instance.FormClosed -= RemoveReference;
+				_instance = null;
+			}
+		}
+
+		protected override void OnClick(EventArgs e)
+		{
+			base.OnClick(e);
+
+			if (_instance == null)
+			{
+				_instance = new T();
+				_instance.FormClosed += RemoveReference;
+				_instance.Show();
+			} else
+			{
+				_instance.Show();
+				_instance.Focus();
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
